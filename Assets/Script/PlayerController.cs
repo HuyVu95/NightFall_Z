a36b93299dd5b402876f5dbc7d3eee7ab7a91de8
Vu Duc Huy <vdhuy1995@gmail.com>
2025-09-27 17:15:25 +0700

    ok2

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D rb;
     public float speed = 3f;
     private Vector2 moveInput;
+    public int facingDirection = 1;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +21,15 @@ public class PlayerController : MonoBehaviour
         moveInput.x = Input.GetAxisRaw("Horizontal");
         moveInput.y = Input.GetAxisRaw("Vertical");
         moveInput.Normalize();
+        if (moveInput.x > 0 && transform.localScale.x < 0 || moveInput.x < 0 && transform.localScale.x > 0)
+        {
+            Flip();
+        }
+    }
+    void Flip()
+    {
+        facingDirection *= -1;
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
     }
     void FixedUpdate()
     {
