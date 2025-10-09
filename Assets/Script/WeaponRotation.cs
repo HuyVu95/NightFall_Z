d6701fabf4bdd19e7a3c0c2d97feb3673b4413e3
Vu Duc Huy <vdhuy1995@gmail.com>
2025-10-10 00:01:00 +0700

    ok day 9

--- Assets/Script/WeaponRotation.cs
+++ Assets/Script/WeaponRotation.cs
@@ -4,27 +4,51 @@ using UnityEngine;
 
 public class WeaponRotation : MonoBehaviour
 {
+    //public SpriteRenderer weaponSprite;
+    //public Transform weaponPivot;
+
+    //void Update()
+    //{
+    //    Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    //    Vector3 direction = mousePos - transform.position;
+
+
+    //    float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+    //    //weaponPivot.rotation = Quaternion.Euler(0f, 0f, angle);
+    //    //// Nếu chuột nằm phía sau nhân vật (bên trái)
+    //    //if (angle > 90 || angle < -90)
+    //    //{
+    //    //    weaponSprite.flipX = true;
+    //    //    weaponSprite.flipY = true;  // Lật sprite
+    //    //}
+    //    //else
+    //    //{
+    //    //    weaponSprite.flipX = false;
+    //    //    weaponSprite.flipY = false; // Bình thường
+    //    //}
+
+    //}
+    public Transform weaponPivot; // chứa sprite + firepoint
     public SpriteRenderer weaponSprite;
+
     void Update()
     {
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector3 direction = mousePos - transform.position;
+        Vector3 direction = mousePos - weaponPivot.position;
 
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0f, 0f, angle);
-        
+        weaponPivot.rotation = Quaternion.Euler(0f, 0f, angle);
+
+        // Kiểm tra nếu chuột ở phía sau (góc > 90 hoặc < -90)
         if (angle > 90 || angle < -90)
         {
-            transform.localScale = new Vector3(1, 1, -1); // lật Y
+            // Lật toàn bộ vũ khí bằng trục X (không làm dốc)
+            weaponPivot.localScale = new Vector3(-1, 1, 1);
         }
         else
         {
-            transform.localScale = new Vector3(1, 1, 1); // bình thường
+            weaponPivot.localScale = new Vector3(1, 1, 1);
         }
-
-
-
     }
-    
-
 }
